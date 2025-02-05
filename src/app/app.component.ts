4513f010d1110a55a4c470d0fc594fcabd942db4
Arely370 <arelitt.ti21@utsjr.edu.mx>
2025-02-05 00:56:18 -0600

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,71 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true, // Agregar esto si es un componente standalone
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'primerproyecto';
+  title = 'fundamentos';
+
+  // Propiedades.
+  nombre = 'ARELI';
+  aniosServicio = 22;
+  email = 'arelitt21@utsjr.edu.mx';
+  activo = true; 
+  alumnos = [24, 25, 26];
+  contador = 0;
+  numero = 5;
+  desactivarBoton = true;
+  productos = [
+    {
+      id: 1,
+      descripcion: 'Gansito Marinela',
+      precio: 15.50
+    },
+    {
+      id: 2,
+      descripcion: 'Sabritas',
+      precio: 20.75
+    },
+    {
+      id: 3,
+      descripcion: 'Pepsi',
+      precio: 18.00
+    }
+  ];
+
+  // Función que muestra un mensaje dependiendo del valor de la propiedad 'activo'.
+  estaActivo() {
+    if (this.activo) {
+      return 'Empleado activo';
+    } else {
+      return 'Empleado inactivo';
+    }
+  }
+
+  // Método que suma los valores del arreglo de alumnos.
+  sumarAlumnos() {
+    let suma = 0;
+    for (let i = 0; i < this.alumnos.length; i++) {
+      suma += this.alumnos[i];
+    }
+    return suma;
+  }
+
+  // Función para incrementar el contador.
+  incrementarContador() {
+    this.contador++;
+  }
+
+  // Función para decrementar el contador.
+  decrementarContador() {
+    this.contador--;
+  }
+
+  // Función que cambia el valor de desactivarBoton.
+  cambiarValor() {
+    this.desactivarBoton = false;
+  }
 }
